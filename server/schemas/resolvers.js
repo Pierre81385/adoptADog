070209df3ae49339912e8a588048dd90942e9606
Peter Bishop <pjb.den@gmail.com>
2021-07-02 10:41:09 -0600

    typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,12 @@
-const { Dog } = require("../models");
+const { Dog, User } = require("../models");
 
 const resolvers = {
   Query: {
     dogs: async () => {
       return Dog.find();
+    },
+    users: async () => {
+      return User.fina();
     },
   },
 };
