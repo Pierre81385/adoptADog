7d58c3c519b496228e1d03a644bda6d94a649727
Peter Bishop <pjb.den@gmail.com>
2021-07-03 13:53:23 -0600

    added API file to Utils in Client directory for using thedogapi

--- client/src/pages/view.js
+++ client/src/pages/view.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import { Carousel, CarouselItem } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
