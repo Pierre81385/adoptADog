f0d1b6e20fb8fa625e14c5887ac9092d5388afc6
Taylor Smith <tbsmith0205@gmail.com>
2021-07-05 21:53:55 -0600

    added about, contact, and summary info

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -4,7 +4,14 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <p>About Page Here</p>
+      <p>
+        AdoptADog was developed by dog lovers Peter Bishop, Ryan Geisreiter,
+        Joshua Abeyta, and Taylor Smith. We wanted to incorporate our love of
+        dogs into a useful app that could be used to find a dog to adopt for any
+        prospective current or future dog owner. We would love to be able to
+        expand functionality and add sections for cats and other animals based
+        on the location specified of the user in the future.
+      </p>
     </div>
   );
 }
